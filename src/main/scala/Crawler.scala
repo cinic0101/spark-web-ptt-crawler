0205d7ae1f974c0c71d5d052911c4ad1bc317bd0
Derek <chuanfeng.wu@pousheng.com>
2017-08-09 17:24:48 +0800

    Convert parsed data to data frame

--- src/main/scala/Crawler.scala
+++ src/main/scala/Crawler.scala
@@ -9,39 +9,37 @@ object Crawler {
       .appName("PTTCrawler")
       .getOrCreate()
     val sc = sparkSession.sparkContext
+    import sparkSession.implicits._
 
     val domain = sc.broadcast("https://www.ptt.cc")
     val titleRegex = sc.broadcast("""(?<reply>Re:)?\s*\[(?<category>.+)\]\s*(?<title>.+)""".r)
     val linkRegex = sc.broadcast("""/bbs\/\S+\/(?<id>\S+)\.html""".r)
 
-    val urls = sc.textFile(args(0))
-    urls.foreach(url => {
-      val doc = Jsoup.connect(url).cookie("over18", "1").get()
-      //val lastPageUrl = doc.select("div.btn-group-paging > a:contains(上頁)").first().attr("href")
+    val board = args(0)
+    val url = s"https://www.ptt.cc/bbs/$board/index.html"
+    val doc = Jsoup.connect(url).cookie("over18", "1").get()
+    //val lastPageUrl = doc.select("div.btn-group-paging > a:contains(上頁)").first().attr("href")
 
-      val links = doc.select("div.r-ent").iterator().asScala.map(x => {
-        val titleLink = x.select("div.title > a")
-        val titleText = titleLink.text
-        val link = titleLink.attr("href")
+    val latest = doc.select("div.r-ent").iterator().asScala.map(x => {
+      val titleLink = x.select("div.title > a")
+      val titleText = titleLink.text
+      val link = titleLink.attr("href")
 
+      val title = titleRegex.value.findFirstMatchIn(titleText) match {
+        case Some(m) => (if(m.group(1) != null) "1" else "0", m.group(2), m.group(3))
+        case None => ("0", "", titleText)
+      }
+      val id = linkRegex.value.findFirstMatchIn(link) match {
+        case Some(m) => m.group(1)
+        case None => ""
+      }
 
-        val title = titleRegex.value.findFirstMatchIn(titleText) match {
-          case Some(m) => (if(m.group(1) != null) 1 else 0, m.group(2), m.group(3))
-          case None => (0, "", titleText)
-        }
-        val id = linkRegex.value.findFirstMatchIn(link) match {
-          case Some(m) => m.group(1)
-          case None => ""
-        }
+      val date = x.select("div.meta > div.date").text
+      val author = x.select("div.meta > div.author").text
 
-        val date = x.select("div.meta > div.date").text
-        val author = x.select("div.meta > div.author").text
+      Row(title._1, date, id, author, title._2, title._3, if(link.isEmpty) "" else domain.value + link)
+    }).toList.toDF
 
-        (title._1, date, id, author, title._2, title._3, if(link.isEmpty) "" else domain.value + link)
-      })
-
-
-      links.foreach(println)
-    })
+    latest.show()
   }
 }
---
+++ src/main/scala/Row.scala
@@ -0,0 +1,1 @@
+case class Row(reply: String, date: String, id: String, author: String, category: String, title: String, link: String)
